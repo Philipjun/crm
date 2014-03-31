e70496200c195c835a197ff9b78c00ae1b458577
Philip Jun <inbox@philipjun.com>
2014-03-31 08:21:43 -0400

    updated files

--- rolodex.rb
+++ rolodex.rb
@@ -1,28 +1,36 @@
 class Rolodex
 
 	def initialize
-  	contacts= []
+  	@contacts =[]
+    @id = 1000
 	end
 
 	def add_contact(name)
-  	contact = Contact.new
-  	contact.name = name
+  	first = Contact.new
+  	contact.name = contact
   	@contacts << contact
 	end
+
 
 	def display_all_contacts
   	@contacts.each do |contact|
     	puts contact
+      
   	end
   end 
 
 	def delete_contact(id)
-  	@contacts.deleteif {|c| c.id == id}
-	end
-
+  	  @contacts.delete_if { |c| c.id == id} 
+    end
+      
+   def display_id
+      @contacts.each do |contact| puts "ID: #{contact.id}\nFirst Name: #{contact.first_name}\nLast Name: #{contact.last_name}\n--\n"}
+  end
+      
+	 
   def edit_contact(id, new_name)
-		@contact.each do |contact|
-			if contact.id ==id
+		@contacts.each do |contact|
+			if contact.id == id
 				contact.name == new_name
 			end
 		@contacts.delete(result) if result !=nil
@@ -30,5 +38,5 @@ class Rolodex
  
 
   end
-
 end
+
--- runner.rb
+++ runner.rb
@@ -4,49 +4,77 @@ require './rolodex'
 
 class Runner
 	def initializer
+		puts "CRM system"
 		@rolodex =Rolodex.new
 	end
 
 	def main_menu
-		puts "1. Add a contact"
-		puts "2. See all contacts"
-		puts "3. Remove a contact"
-		puts "4. Edit a contact"
-    puts "0. To exit"
+		puts "[1] Add a new contact"
+  	puts "[2] Modify an existing contact"
+  	puts "[3] Delete a contact"
+  	puts "[4] Display all the contacts"
+	  puts "[5] Display an attribute"
+	  puts "[6] Exit"
+	  puts "Enter a number: "
+end
 
-	end
+	
 
 	def add_contact
-		puts "What is your first name?"
-		name = gets.chomp
-    puts "what is your last name?"
-    lastname= gets.chomp
-    puts "what is your email?"
-    email = gets.chomp
-    puts "notes"
-    notes =gets.chomp
+
+			puts "What is your first name?"
+		first_name = gets.chomp
+
+	   puts "What is your last name?"
+	   last_name = gets.chomp
+
+	   puts "What is your email?"
+	   email =gets.chomp
+
+	   puts "Place notes"
+	   notes =gets.chomp
+	   @rolodex.add_contact(first_name, last_name, email, note)
 
 	end
 
-	def show_contacts
-		puts" All contacts"
-		@rolodex.show_contacts
-		puts "----------"
-	end
+	def display_id
+		@rolodex.display_id
+			end
 
-	def remove_contact
-		puts"Enter contact to be remmoved"
+
+	def display_all_contacts
+		puts "All contacts"
+		@rolodex.display_all_contacts
+		end
+
+	def delete_contact
+		puts "Enter contact's ID to be removed"
 		id = gets.chomp.to_i
-		@rolodex.remove_contact(id)
+		@rolodex.delete_contact(id)
 	end
 
 	def edit_contact
 		puts"ID to edit"
+		display_id
 		id = gets.chomp.to_i
-		puts "Enter new contact name"
-		new_name= gets.chomp
-		@rolodex.edit_contact(id, new)
+		puts "What do you want to edit?"
+		puts "(1) First Name"
+		puts "(2) Last Name"
+		puts "(3) Email"
+		puts "(4) Note"
+		puts "(0) Go back to Main Menu"
+
+		input = gets.chomp.to_i
+		if input != 0
+		puts "Are you sure?"
+		new_info =gets.chomp
+	@rolodex.edit_contact(id, new_name)
+	puts "new info"
+		@rolodex.display_target(id)
 	end
+
+
+end
 
 	def run
 		done = false
@@ -55,22 +83,29 @@ class Runner
 		input= gets.chomp.to_i
 			if input == 0
 				done = true
-			elsif  if input == 1
+
+			elsif input == 1
 				add_contact
 			elsif input == 2
+				modify_contact
+			elsif input == 3
+				delete_contact
+			elsif input == 4
 				display_all_contacts
-			elsif input ==3
-				remove_contact
-			elsif  input ==4
-				edit_contact
+			elsif input == 5
+				display_attribute
+			elsif input == 6
+				done = true
+			else
+				puts "Please enter the correct input"
 
-  			
 
 				end
 			end
+
 		end
 	end
-end
+
 
 runner = Runner.new
 runner.run
