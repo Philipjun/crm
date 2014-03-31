3f4422c910fb0572b0a82027957e613354e5182f
Philip Jun <inbox@philipjun.com>
2014-03-31 00:36:28 -0400

    Contact class updated

--- contact.rb
+++ contact.rb
@@ -1,42 +1,22 @@
 class Contact
 
-	attr_accessor :notes, :name, :id
+@@counter = 1000
+
+	attr_accessor :notes, :name, :id, :last_name
 	
-	@@counter = 1000
 
 	def initialize
 		@id = Contact.get_id
-	end
-
-	def first_name
-
-	end
-
-	def last_name
-
-	end
-
-	def email
-
 		end
 
-	def notes
-
-	end
-
-	def id
-
-	end
-
-
-
-	def to_s
-		"Name: #{@id}\nName: #{@name}\n #{@notes}"
-	end
 
 	def self.get_id
 		@@counter += 1
 		@@counter
+	end
+
+def to_s
+		"ID: #{@id}\nName: #{@name}\nNotes: #{@notes}"
 	end
 
 end
